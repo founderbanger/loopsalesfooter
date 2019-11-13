19cbf99f122da557889b67c826099763e3a13211
Cody <founderbanger@users.noreply.github.com>
2019-11-12 20:06:49 -0800

    Update loopsalesfooter.js

--- loopsalesfooter.js
+++ loopsalesfooter.js
@@ -1,7 +1,7 @@
 window.onload = function(){
-    $("#footerDiv").html(`<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-148756277-2"></script>
-<script>
+    
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
   $(window).on('load', function(){ /*code here*/ 
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
@@ -9,33 +9,13 @@ window.onload = function(){
 
   gtag('config', 'UA-148756277-2');
     });
-</script>
 
-<meta name="myblogu-verification" content="Yzg4Y2QzMjc1MmU5ZjM4NTQxZWJhNzFmNzU4YTZhMTA=" />
-<!-- Screenshot Tool Required Scripts -->
-        <script src="https://cdn.jsdelivr.net/gh/founderbanger/feedback@latest/src/feedback.min.js"></script> 
-        <script src="//cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.js"></script>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/founderbanger/feedback@latest/src/feedback.css"/>
 
-        <!-- Screenshot Tool Script -->
-        <script type="text/javascript">
-          $(window).on('load', function(){ /*code here*/ 
-                $.feedback({
-                    endpoint: "https://app.loop.best/campgrounds/5d76c57015012f0016a27fe1/comments",
-                    initButtonText: "Give Feedback",
-                    
-                });
-            });
-        </script>
-
-<script>
   window.intercomSettings = {
     app_id: "qgjl682x"
   };
-</script>
 
-<script>
   (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/qgjl682x';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
 
-</script>`)
+
 }
