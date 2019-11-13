be4faef2f0c5d843b459e96063af777a0f7f527e
Cody <founderbanger@users.noreply.github.com>
2019-11-12 19:40:18 -0800

    Update and rename loopsalesfooter.html to loopsalesfooter.js

--- loopsalesfooter.js
+++ loopsalesfooter.js
@@ -1,4 +1,5 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
+window.onload = function(){
+    $("#footerDiv").html(`<!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-148756277-2"></script>
 <script async=false>
   $(window).on('load', function(){ /*code here*/ 
@@ -36,4 +37,5 @@
 <script>
   (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/qgjl682x';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
 
-</script>
+</script>`)
+}
