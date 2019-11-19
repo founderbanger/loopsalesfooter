1473b0c00f670568f91a35ea384bc5267879d09d
Cody <founderbanger@users.noreply.github.com>
2019-11-18 16:09:23 -0800

    Update loopsalesfooter.js

--- loopsalesfooter.js
+++ loopsalesfooter.js
@@ -6,11 +6,11 @@
   gtag('config', 'UA-148756277-2');
 
 
-  window.intercomSettings = {
+  /*window.intercomSettings = {
     app_id: "qgjl682x"
   };
 
   (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/qgjl682x';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
 
-
+*/
 
