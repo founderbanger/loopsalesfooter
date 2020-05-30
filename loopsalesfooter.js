d9710845caf8a94e536b8aa56afa7f0291ff0bea
Cody <founderbanger@users.noreply.github.com>
2020-05-29 18:02:29 -0700

    Update loopsalesfooter.js

--- loopsalesfooter.js
+++ loopsalesfooter.js
@@ -1,9 +1,9 @@
 
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+  //window.dataLayer = window.dataLayer || [];
+  //function gtag(){dataLayer.push(arguments);}
+  //gtag('js', new Date());
 
-  gtag('config', 'UA-148756277-2');
+  //gtag('config', 'UA-148756277-2');
 
 
 function detectmob() { 
