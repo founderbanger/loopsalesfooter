2c623d1f5c73528f7fd7d52754e8bc48906166f4
Cody <founderbanger@users.noreply.github.com>
2020-07-27 10:18:25 -0700

    Update loopsalesfooter.js

--- loopsalesfooter.js
+++ loopsalesfooter.js
@@ -24,21 +24,13 @@ function detectmob() {
 var is_mobile = detectmob();
 
 if (is_mobile == false) {
+  $('body').append('<script type=“text/javascript” id=“hs-script-loader” async defer src=“//js.hs-scripts.com/7620719.js”></script>')
  /* window.intercomSettings = {
     app_id: "qgjl682x",
-    //hide_default_launcher: is_mobile*/
+    //hide_default_launcher: is_mobile
   };
 
   (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/qgjl682x';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
+*/
 }
-/*
-(function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:1572671,hjsv:6};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-    */
 
