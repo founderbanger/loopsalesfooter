42a4b849ba71a22644964b06b8d0a8d5f4b0cba6
Cody <founderbanger@users.noreply.github.com>
2020-07-27 11:12:30 -0700

    Update loopsalesfooter.js

--- loopsalesfooter.js
+++ loopsalesfooter.js
@@ -24,13 +24,11 @@ function detectmob() {
 var is_mobile = detectmob();
 
 if (is_mobile == false) {
-  $('body').append(`<script type=“text/javascript” id=“hs-script-loader” async defer src=“//js.hs-scripts.com/7620719.js”></script>`)
- /* window.intercomSettings = {
-    app_id: "qgjl682x",
-    //hide_default_launcher: is_mobile
-  };
+  // HubSpot Script Loader. Please do not block this resource. See more: http://hubs.ly/H0702_H0
 
-  (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/qgjl682x';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
-*/
+!function(t,e,r){if(!document.getElementById(t)){var n=document.createElement("script");for(var a in n.src="https://js.hscollectedforms.net/collectedforms.js",n.type="text/javascript",n.id=t,r)r.hasOwnProperty(a)&&n.setAttribute(a,r[a]);var i=document.getElementsByTagName("script")[0];i.parentNode.insertBefore(n,i)}}("CollectedForms-7620719",0,{"crossorigin":"anonymous","data-leadin-portal-id":7620719,"data-leadin-env":"prod","data-loader":"hs-scriptloader","data-hsjs-portal":7620719,"data-hsjs-env":"prod"});
+!function(t,e,r){if(!document.getElementById(t)){var n=document.createElement("script");for(var a in n.src="https://js.hs-banner.com/7620719.js",n.type="text/javascript",n.id=t,r)r.hasOwnProperty(a)&&n.setAttribute(a,r[a]);var i=document.getElementsByTagName("script")[0];i.parentNode.insertBefore(n,i)}}("cookieBanner-7620719",0,{"data-loader":"hs-scriptloader","data-hsjs-portal":7620719,"data-hsjs-env":"prod"});
+!function(e,t){if(!document.getElementById(e)){var c=document.createElement("script");c.src="//js.hs-analytics.net/analytics/1595873400000/7620719.js",c.type="text/javascript",c.id=e;var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(c,n)}}("hs-analytics");
+!function(t,e,r){if(!document.getElementById(t)){var n=document.createElement("script");for(var a in n.src="https://js.usemessages.com/conversations-embed.js",n.type="text/javascript",n.id=t,r)r.hasOwnProperty(a)&&n.setAttribute(a,r[a]);var i=document.getElementsByTagName("script")[0];i.parentNode.insertBefore(n,i)}}("hubspot-messages-loader",0,{"data-loader":"hs-scriptloader","data-hsjs-portal":7620719,"data-hsjs-env":"prod"});
 }
 
